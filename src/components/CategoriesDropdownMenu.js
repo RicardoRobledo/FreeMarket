e603fe10b3b8d94ea36015f50da95ea5daf0f5cb
RicardoRobledo <RicardoProgrammer44@gmail.com>
2023-03-11 18:49:02 -0600

    feat(main): 'CategoriesDrowdonMenu' was added

---
+++ src/components/CategoriesDropdownMenu.js
@@ -0,0 +1,30 @@
+import '../css/CategoriesDropdownMenu.css';
+
+import React from 'react';
+
+
+export default class CategoriesDropdownMenu extends React.Component {
+
+  render() {
+    return (
+      <>
+			  <nav> 
+				  <li><a href="">Categorias</a>
+					  <ul>
+						  <li><a href="">Vehicles</a></li>
+						  <li><a href="">Supermarket</a></li>
+              <li><a href="">Technology</a></li>
+              <li><a href="">Home and forniture</a></li>
+              <li><a href="">Home appliances</a></li>
+              <li><a href="">Fashion</a></li>
+              <li><a href="">Sports and fitness</a></li>
+					    <li><a href="">Tools</a></li>
+              <li><a href="">Construction</a></li>
+            </ul>
+				  </li>
+        </nav>
+      </>
+    );
+  }
+
+}
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,31 +2,53 @@ import '../css/Header.css'
 
 import React from 'react';
 
-import { FaShopify, FaSearch } from "react-icons/fa";
+import { FaShopify, FaSearch, FaSearchLocation, FaCartPlus } from "react-icons/fa";
+import CategoriesDropdownMenu from './CategoriesDropdownMenu';
 
 
-export default class Menu extends React.Component{
+export default class Menu extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        
-        {/*Icon*/}
-        <FaShopify size={40}/>
+      <div>
+        <div className="container">
+          {/*Icon*/}
+          <FaShopify size={40} />
 
-        {/*Search bar*/}
-        <div>
+          {/*Search bar*/}
           <form>
             <input type="text" placeholder="Search" className="search-input"></input>
             <button type="submit" className="search-button">
-              <FaSearch size={20}/>
+              <FaSearch size={20} />
             </button>
           </form>
+
+          {/*Announcement*/}
+          <h1>Save money here</h1>
         </div>
 
-        {/*Announcement*/}
-        <h1>Save money here</h1>
-     
+        <div className="container">
+          <h4><FaSearchLocation/>Enter your address</h4>
+
+          <ul>
+            <h4>
+              <CategoriesDropdownMenu/>
+            </h4>
+            <h4>Sale</h4>
+            <h4>Historic</h4>
+            <h4>Supermarket</h4>
+            <h4>Fashion</h4>
+            <h4>Sell</h4>
+            <h4>Help</h4>
+          </ul>
+
+          <ul>
+            <h4>Sign in</h4>
+            <h4>Log in</h4>
+            <h4>My sales</h4>
+            <FaCartPlus className="cart"/>
+          </ul>
+        </div>
       </div>
     );
   }
