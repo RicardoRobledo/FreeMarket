e024a55f8d504823d86cfd0a2d2c22dc669f29d8
RicardoRobledo <RicardoProgrammer44@gmail.com>
2023-03-13 21:15:28 -0600

    feat(main): 'react-router-dom' was added

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+
+export default class Login extends React.Component {
+
+  render(){
+    return (
+      <Fragment>
+        <h1>1</h1>
+      </Fragment>
+    );
+  }
+
+}
