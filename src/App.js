dca4e39c1d7c8f3d8ae7c0459f194d9d42ae694a
RicardoRobledo <RicardoProgrammer44@gmail.com>
2023-03-13 21:36:23 -0600

    feat(main): 'BrowserRouter' was added in 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './css/App.css';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 
 function App() {
 
   return (
-    <Home/>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 
 }
