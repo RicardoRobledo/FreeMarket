8867e84280fabb27ed8f13f08449e2f461df0f0a
RicardoRobledo <RicardoProgrammer44@gmail.com>
2023-03-16 15:15:01 -0600

    feat(main): 'LoginForm' was added in 'Login' page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,46 @@
 import '../css/LoginForm.css';
 
-import React, { Fragment } from 'react';
+import React from 'react';
+
+import { FaGuitar, FaXbox, FaDrum, FaFootballBall } from "react-icons/fa";
 
 
 export default class Login extends React.Component {
 
   render(){
     return (
-      <Fragment>
-        <Menu showSearch={true}/>
-      </Fragment>
+      <div class="login-container">
+        <div className="announcement-card">
+          <h2>Welcome to FreeMarket<br/></h2>
+          <hr/>
+          <p>
+            Find all what you need by reachable prices<br/>
+            from videogames to instruments
+          </p>
+          <div className="icons-container">
+            <ul>
+              <li><FaGuitar color="brown"/></li>
+              <li><FaXbox color="green"/></li>
+              <li><FaDrum color="gray"/></li>
+              <li><FaFootballBall color="red"/></li>
+            </ul>
+          </div>
+        </div>
+        <form className="login-form">
+          <ul>
+            <li><label>Username</label></li>
+            <li><input className="login-input" type="text field"></input></li>
+            <li><label>Password</label></li>
+            <li><input className="login-input" type="text field"></input></li>
+            <li>
+              <div className="buttons">
+                <button className="button-login-form">Log in</button>
+                <a className="signup-login-form">Sign up</a>
+              </div>
+            </li>
+          </ul>
+        </form>
+      </div>
     );
   }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import Menu from '../components/Menu';
+import LoginForm from '../components/LoginForm'
 
 import React, { Fragment } from 'react';
 
@@ -8,7 +9,8 @@ export default class Login extends React.Component {
   render(){
     return (
       <Fragment>
-        <Menu showSearch={true}/>
+        <Menu showSearch={false}/>
+        <LoginForm/>
       </Fragment>
     );
   }
